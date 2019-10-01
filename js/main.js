55394084ace175638639e58351e976bd370f1bf7
DzmitryLitvinau <angeridze93@gmail.com>
2019-10-01 21:43:14 +0300

    added active page and adress

--- js/main.js
+++ js/main.js
@@ -9,10 +9,13 @@ var DESCRIPTION = 'Стильная квартира в жилом небоск�
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var RANDOM_ADVERTS_AMOUNT = 8;
 var MAP_OVERLAY_XSIZE = 1200;
-var MAP_PIN_XSIZE = 50;
 var PIN_ELEM_XSIZE = 50;
 var PIN_ELEM_YSIZE = 70;
-
+var MAIN_PIN_XSIZE = 62;
+var MAIN_PIN_YSIZE = 84;
+var MAIN_PIN_XCOORD = 570;
+var MAIN_PIN_YCOORD = 375;
+var ENTER_KEYCODE = 13;
 var getRandomIntInclusive = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -37,7 +40,7 @@ var getRandomAdvert = function (avatarNumber) {
   randomAdvert.offer.features = FEATURES.slice(0, getRandomIntInclusive(1, FEATURES.length));
   randomAdvert.offer.description = DESCRIPTION.slice(0, getRandomIntInclusive(0, DESCRIPTION.length - 1));
   randomAdvert.offer.photos = PHOTOS.slice(0, getRandomIntInclusive(1, PHOTOS.length));
-  randomAdvert.location.x = getRandomIntInclusive(0, MAP_OVERLAY_XSIZE - MAP_PIN_XSIZE);
+  randomAdvert.location.x = getRandomIntInclusive(0, MAP_OVERLAY_XSIZE);
   randomAdvert.location.y = getRandomIntInclusive(130, 630);
   randomAdvert.offer.address = '{{location.x}}, {{location.y}}'.replace('{{location.x}}, {{location.y}}', randomAdvert.location.x + ',' + ' ' + randomAdvert.location.y);
 
@@ -55,7 +58,7 @@ var getRandomSpecsArr = function () {
 
 var randomAdverts = getRandomSpecsArr();
 var map = document.querySelector('.map');
-map.classList.remove('map--faded');
+/* map.classList.remove('map--faded'); */
 
 var similarPinElement = map.querySelector('.map__pins');
 
@@ -109,3 +112,68 @@ for (var i = 0; i < randomAdverts.length; i++) {
   fragment.appendChild(renderPin(randomAdverts[i]));
 }
 similarPinElement.appendChild(fragment);
+
+var adForm = document.querySelector('.ad-form');
+var fieldsetsAdForm = adForm.querySelectorAll('fieldset');
+var mapFilters = document.querySelector('.map__filters');
+var mainPin = document.querySelector('.map__pin--main');
+var adressInput = document.querySelector('#address');
+var housingRoomsSelect = document.querySelector('#room_number');
+var housingGuestsSelect = document.querySelector('#capacity');
+// Бред который не получился --------------------------------
+var onChangeRoomClick = function (room, guests) {
+  room.addEventListener('change', function () {
+    for (var j = 0; j < guests.options.length; j++) {
+      if (room.options[i].selected) {
+        for (var k = 0; k < guests.options.length; k++) {
+          if (Number(room.options[j].value) < Number(guests.options[k].value)) {
+            guests.options[k].setAttribute('disabled', 'disabled');
+          }
+          if (Number(room.options[j].value) !== 100) {
+            guests.options[3].setAttribute('disabled', 'disabled');
+          }
+        }
+      }
+    }
+  }
+  );
+};
+onChangeRoomClick(housingRoomsSelect, housingGuestsSelect);
+// -------------------------------------------------------------
+
+adressInput.setAttribute('value', (MAIN_PIN_XCOORD + (MAIN_PIN_XSIZE / 2)) + ',' + ' ' + (MAIN_PIN_YCOORD + (MAIN_PIN_YSIZE - (MAIN_PIN_YSIZE - MAIN_PIN_XSIZE)) / 2));
+
+var disableElements = function (element) {
+  for (var j = 0; j < element.length; j++) {
+    element[i].setAttribute('disabled', 'disabled');
+  }
+
+};
+var enableElements = function (element) {
+  for (var k = element.length - 1; k >= 0; k--) {
+    element[i].removeAttribute('disabled');
+  }
+  adressInput.setAttribute('disabled', 'disabled');
+};
+
+var getActiveMode = function () {
+  map.classList.remove('map--faded');
+  adForm.classList.remove('ad-form--disabled');
+  mapFilters.classList.remove('map__filters--disabled');
+  enableElements(fieldsetsAdForm);
+  enableElements(mapFilters.children);
+  adressInput.setAttribute('value', (MAIN_PIN_XCOORD + (MAIN_PIN_XSIZE / 2)) + ',' + ' ' + (MAIN_PIN_YCOORD + MAIN_PIN_YSIZE));
+};
+
+adForm.classList.add('ad-form--disabled');
+mapFilters.classList.add('map__filters--disabled');
+disableElements(mapFilters.children);
+disableElements(fieldsetsAdForm);
+
+
+mainPin.addEventListener('mousedown', getActiveMode);
+mainPin.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    getActiveMode();
+  }
+});
